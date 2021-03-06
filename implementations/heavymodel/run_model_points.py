4d4f93fb2c56331ff76aeb74b630289b7afd2549
lewisfogden <lewisfogden@users.noreply.github.com>
2021-03-06 10:24:31 +0000

    Update run_model_points.py

--- implementations/heavymodel/run_model_points.py
+++ implementations/heavymodel/run_model_points.py
@@ -26,5 +26,46 @@ def get_premium(row):
     except Exception as err:
         return repr(err)
 
+#def npv(cashflow, yield_curve):
+#    pv = 0.0
+#    for t in range(len(cashflow.values)):
+#        pv += yield_curve.v[t] * cashflow(t)
+#    return pv
+
+def get_npv(row, model_variables):
+    # We call the model directly here as we want to get several NPVs without recalculating it all
+    try:
+        data = Data(row)
+        model = TermAssurance(data, pricing_basis)
+        proj_term = data.term_y * 12 + 1
+        model._run(proj_len = proj_term)
+        results = {}
+        for field_name in model_variables:
+            field_object = getattr(model, field_name)
+            results[field_name] = pricing_basis.rfr.npv(field_object, proj_term)
+        return results
+    except Exception as err:
+        return repr(err)
+
+
+# %%
+
 df_model_points["monthly_premium"] = df_model_points.apply(get_premium, axis=1)
+df_model_points["annual_premium"] = df_model_points["monthly_premium"] * 12
+
+# %%
+
+
+output_columns = ["net_cf", "claims", "premiums"]
+
+#df_model_points2 = df_model_points.apply(func=lambda row: get_npv(row, output_columns),
+#                                         result_type="expand",
+#                                         axis=1)
+
+# %%
+
+df_model_points2 = df_model_points.apply(func=get_npv,
+                                         model_variables = output_columns,
+                                         result_type="expand",
+                                         axis=1)
 # %%
